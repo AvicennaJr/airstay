0d3db39b778d6e6fdba8f38387c1309ef7f4ed86
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-05-19 11:50:23 +0300

    clean repo

--- js/map.js
+++
@@ -1,6 +0,0 @@
-// map.js
-var map = L.map('map').setView([-6.16637, 39.5039], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18
-}).addTo(map);
