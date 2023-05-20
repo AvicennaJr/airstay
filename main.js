55a40403e13aa0d9b3b5176f7a21f310e6129a0d
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-05-21 00:48:31 +0300

    add map interactivity

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Map Initialization
 var map = L.map('map',{
-    scrollWheelZoom: false,
-    zoomControl: false,
-    dragging: false,
-    touchZoom: false,
-    tap: false
+    scrollWheelZoom: true,
+    zoomControl: true,
+    dragging: true,
+    touchZoom: true,
+    tap: true
 }).setView([-6.16600, 39.5000], 10);
 // var map = L.map('map').setView([-6.141558, 39.334626],10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
